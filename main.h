b84d76b52466ec958541a60adda8d3a65f695020
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-24 04:04:16 +0100

    fixes in main.h

--- main.h
+++ main.h
@@ -4,13 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-<<<<<<< HEAD
-
-=======
 #include <limits.h>
 #include <unistd.h>
 #include "_putchar.c"
->>>>>>> e90015a3b2e583bb5bfc376032e4e1c2d9b46e8b
 
 /**
  * struct format - Struct for format
@@ -24,9 +20,6 @@ typedef struct specifiers
 	int (*f)(va_list);
 } specifiers_t;
 
-#include "get_print_func.c"
-#include "print_buf.c"
-#include "handl_buf.c"
 
 int _printf(const char *format, ...);
 int get_function(char s, va_list args);
@@ -37,5 +30,14 @@ int print_string(va_list args);
 int print_digit(va_list args);
 int print_mod(va_list args);
 int print_rev_string(va_list args);
+#include "get_function.c"
+#include "print_buf.c"
+#include "handl_buf.c"
+#include "print_char.c"
+#include "print_string.c"
+#include "print_digit.c"
+#include "print_rev_string.c"
+#include "print_mod.c"
+
 
 #endif
