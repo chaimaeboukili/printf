b3f76937112e3624bfa4193c8f69678134d56d79
chaimae <cboukili@hotmail.fr>
2023-04-25 11:23:24 -0700

    changes

--- print_rev_string.c
+++ print_rev_string.c
@@ -7,20 +7,15 @@
 int print_rev_string(va_list args)
 {
 
-	int count_fun = 0, i = 0;
-	char *s = va_arg(args, char *);
+	char *s = va_arg(args, char*);
+	int i;
+	int j = 0;
 
-	if (!s)
+	if (s == NULL)
 		s = "(null)";
-	while (s[i])
-	{
-		i++;
-	}
-	while (i >= 0)
-	{
-		count_fun += _putchar(s[i]);
-		i--;
-	}
-	count_fun--;
-	return (count_fun);
+	while (s[j] != '\0')
+		j++;
+	for (i = j - 1; i >= 0; i--)
+		_putchar(s[i]);
+	return (j);
 }
